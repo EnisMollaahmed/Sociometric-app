f03eaa7a3d52e55d9eb2454a8b4abcfcc7a780af
Enis Mollaahmed <92330428+EnisMollaahmed@users.noreply.github.com>
2025-05-19 23:03:28 +0300

    Update app.ts

--- sociometric-backend/src/app.ts
+++ sociometric-backend/src/app.ts
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: '*', // Only for testing!
+    origin: 'sociometric-app.vercel.app',
     credentials: true
   }));
 app.use(helmet());
