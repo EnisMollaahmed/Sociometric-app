48a0d7a0973952e74778c2582c61ff752357cbe9
EnisMollaahmed <enis_super02@outlook.com>
2025-05-20 17:54:37 +0300

    added options to cors in app.ts

--- sociometric-backend/src/app.ts
+++ sociometric-backend/src/app.ts
@@ -16,7 +16,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "*" ,methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],allowedHeaders: ['Content-Type', 'Authorization']}));
 app.use(helmet());
 app.use(morgan('dev'));
 
